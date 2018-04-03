4b5d03de18909f13427cc471509e2d34cf19ef8e
WorldWideDev <dev206@gmail.com>
2018-04-02 19:01:16 -0700

    models/gen files code

---
+++ 4_Week/1_Session/HelloModels/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HelloModels.Models;
+
+namespace HelloModels.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            // List<Dictionary<string, object>>
+            ViewBag.Players = DbConnector.Query("SELECT * FROM players");
+            return View();
+        }
+        [HttpPost("create")]
+        public IActionResult Create(Player player)
+        {
+            // Insert new player
+            // write sql insert query
+            string insertQuery = $@"
+                INSERT INTO players (first_name, last_name, number) 
+                VALUES ('{player.FirstName}', '{player.LastName}', '{player.Number}')
+            ";
+            DbConnector.Execute(insertQuery);
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ 4_Week/1_Session/HelloModels/Models/Person.cs
@@ -0,0 +1,9 @@
+namespace HelloModels.Models
+{
+    public class Player
+    {
+        public string FirstName {get;set;}
+        public string LastName {get;set;}
+        public int Number {get;set;}
+    }
+}
