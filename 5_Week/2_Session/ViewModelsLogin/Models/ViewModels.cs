56f3f15dc5503f3a942026285bbed5ad4f12a954
WorldWideDev <dev206@gmail.com>
2018-04-12 17:04:52 -0700

    view models/partials lecture code

---
+++ 5_Week/2_Session/ViewModelsLogin/Models/ViewModels.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace ModelsContinued.Models
+{
+    public class LoginRegModel
+    {
+        public User Registration {get;set;}
+        public LogUser Login {get;set;}
+    }
+}
